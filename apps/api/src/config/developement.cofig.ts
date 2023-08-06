1708563e1267ec5582d3f26a9427cc659e2d31bd
luckyklyist <puspaanupam@gmail.com>
2023-08-06 22:13:52 +0545

    added env to local db

--- apps/api/src/config/developement.cofig.ts
+++ apps/api/src/config/developement.cofig.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Config {
     PORT: number;
     SECRET_KEY: string;
+    LOCAL_DB: string
 }
 
 const config: Config = {
     PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-    SECRET_KEY: process.env.SECRETE_KEY || "RunForestRun"
+    SECRET_KEY: process.env.SECRETE_KEY || "RunForestRun",
+    LOCAL_DB: process.env.LOCAL_DB || "mongodb://localhost:27017/ecomapp"
 }
 
 export default config;
