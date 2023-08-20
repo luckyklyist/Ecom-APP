7b6ae36f7d1159b10a01c9cff8c676eede915db7
luckyklyist <puspaanupam@gmail.com>
2023-08-20 22:40:54 +0545

    ui changed from daisyUI cdn

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Navbar } from "ui";
+import "../styles/globals.css";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,9 @@ export default function RootLayout({
     <>
       <html lang="en">
         <body>
+          <script src="https://cdn.tailwindcss.com"></script>
+          <link href="https://cdn.jsdelivr.net/npm/daisyui@3.5.1/dist/full.css" rel="stylesheet" type="text/css" />
+          <script src="https://cdn.tailwindcss.com"></script>
           <Navbar />
           {children}
         </body>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,16 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-     Home Page
-
-     
+      <div className="card lg:card-side bg-primary-focus shadow-xl conatiner m-6">
+        <img src="https://static.nike.com/a/images/t_default/99486859-0ff3-46b4-949b-2d16af2ad421/custom-nike-dunk-high-by-you-shoes.png" alt="Album" width={300} />
+        <div className="card-body ml-10">
+          <h2 className="card-title text-5xl text-warning font-bold ">Welcome to ECOM NEPAL</h2>
+          <p className="text-sm">Find the best deal in Nepal !!</p>
+          <div className="card-actions justify-end mr-6">
+            <button className="btn btn-neutral">Explore</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
