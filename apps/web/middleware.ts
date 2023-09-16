cf7369c3883a21eaf30e218c9be36e39ce608367
luckyklyist <puspaanupam@gmail.com>
2023-09-16 13:50:32 +0545

    added the middle ware to make the routes protected

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,5 +1,28 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next();
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const token = request.cookies.get("token")?.value;
+  const isPublicPath = path === "/login" || path === "/signup";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
 }
+
+export const config = {
+  matcher: [
+    "/",
+    "/products",
+    "/products/:path*",
+    "/login",
+    "/signup",
+    "/cart",
+    "/profile",
+  ],
+};
